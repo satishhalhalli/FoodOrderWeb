58209da522bb5cbb9cf91b1e3985dd600e423ed8
satishhalhalli <satishhalhalli7484@gmail.com>
2023-03-18 14:07:49 +0530

    worling with cacrt open and cart close

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Header from "./components/Layout/Header";
 import Cart from "./components/Cart/Cart";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Meals from "./components/Meals/Meals";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart/>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
       <main>
-      <Meals/>
+        <Meals />
       </main>
     </Fragment>
   );
-};
+}
+
 export default App;
